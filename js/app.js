53c043707e62887fbdeb41cd4c13ed8e7bfafae5
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2020-08-31 18:33:13 +0530

    alert

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 // document.getElementById("date").innerHTML('Date()');
 const date = (document.getElementById(
-    "date"
-  ).innerHTML = new Date().getFullYear());
-  
+  "date"
+).innerHTML = new Date().getFullYear());
+
+
+function message() {
+  alert("We are working on this page");
+}
+
